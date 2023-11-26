06ee73f1d502c4fe96c187449a8d51c0990ce753
unknown <rinoally@gmail.com>
2023-11-26 15:45:11 +0300

    add commit

--- src/reducer.js
+++ src/reducer.js
@@ -1,24 +1,23 @@
 const initialState = {
     value: 0
-}
+};
 
 export const counterReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'counter/increment':
-                return {
-                    ...state,
-                    value: state.value + 1
-                }
-                
-        case 'counter/decrement':
-                return {
-                    ...state,
-                    value: state.value -  1
-                }
-
-        default:
-            return {
-                ...state
-            }
-    }
+ switch (action.type) {
+    case 'counter/increment': 
+        return {
+            ...state, 
+         value: state.value + 1
+        }
+    case 'counter/decrement': 
+        return {
+            ...state, 
+         value: state.value - 1
+        }
+         
+    default:
+        return {
+            ...state
+        }
+ }
 }
